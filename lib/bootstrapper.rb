0ef8bf82fc7c3e8fc1ef9f61ea81527c407c7d5d
Mike Ruby <emikebr@gmail.com>
2011-09-12 00:28:57 -0500

    added support for rails 3

--- lib/bootstrapper.rb
+++ lib/bootstrapper.rb
@@ -1,5 +1,4 @@
 require "bootstrapper/version"
 
 module Bootstrapper
-  
 end
--- lib/bootstrapper/rails/railtie.rb
+++ lib/bootstrapper/rails/railtie.rb
@@ -1,5 +1,18 @@
 module Bootstrapper
   module Rails
     
+    class Railtie < ::Rails::Railtie
+      config.before_configuration do
+        if ::Rails.root.join("public/stylesheets/bootstrap.css").exist?
+          bootstrap_defaults = %w(bootstrap)
+          bootstrap_defaults.map!{ |a| a + ".min" } if ::Rails.env.production?
+        else
+          bootstrap_defaults = ::Rails.env.production? %w(bootstrap.min) : %w(bootstrap)
+        end
+        
+        config.action_view.stylesheet_expansions[:bootstrap] |= boostrap_defaults
+      end
+    end
+    
   end
 end
